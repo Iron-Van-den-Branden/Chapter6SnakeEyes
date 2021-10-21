6a5fba70c98b1e53e09114532351133a06a61f9a
Iron Van den Branden <iron.vandenbranden@student.hogent.be>
2021-10-21 10:37:06 +0200

    copy from other, clean-up boiler

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Dice
+    {
+        #region Fields
+        private Random _randomizer;
+        #endregion
+        #region Properties
+        public int Dots { get; set; }
+        #endregion
+        #region Constructor
+        public Dice()
+        {
+            _randomizer = new Random();
+            Dots = 6;
+        }
+        #endregion
+        #region Methods
+        public void Roll()
+        {
+            Dots = _randomizer.Next(6) + 1;
+        }
+        #endregion
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class Game
+    {
+        #region Fields
+        private List<int> _highscores = new List<int>();
+        private Dice _dice1;
+        private Dice _dice2;
+        #endregion
+        #region Properties
+        public int Eye1 { get { return _dice1.Dots; } }
+        public int Eye2 { get { return _dice2.Dots; } }
+        public bool HasSnakeEyes { get { return (Eye1 == 1 && Eye2 == 1); } }
+        public IReadOnlyList<int> HighScores { get { return _highscores; } }
+        private int Total { get; set; }
+
+        #endregion
+        #region Constructor
+        public Game()
+        {
+            initialize();
+        }
+        #endregion
+        #region Methods
+
+        private void initialize()
+        {
+            _dice1 = new Dice();
+            _dice2 = new Dice();
+        }
+        public void Play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+            if (HasSnakeEyes)
+            {
+                _highscores.Add(Total);
+                Total = 0;
+            } else
+            {
+                Total += Eye1 + Eye2;
+            }
+        }
+        public void Restart()
+        {
+            initialize();
+
+        }
+        #endregion
+    }
+}
